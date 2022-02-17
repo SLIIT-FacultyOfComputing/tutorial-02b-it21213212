6144a2e736f1be20ab339dde6033d22ab09d476a
Dinusha <99312176+it21213212@users.noreply.github.com>
2022-02-17 15:02:27 +0000

    complete_tute4

--- tute04.cpp
+++ tute04.cpp
@@ -13,17 +13,44 @@ Do not modify the main function.*/
 
 #include <iostream>
 
-long Factorial(int no);
-long nCr(int n, int r);
+long Factorial(int no); // call the function
+long nCr(int n, int r); // call the function
 
 int main() {
   int n, r;
-  std::cout << "Enter a value for n ";
+
+  std::cout << "Enter a value for n "; //get user inputs
   std::cin >> n;
-  std::cout << "Enter a value for r ";
+
+  std::cout << "Enter a value for r "; //get user inputs
   std::cin >> r;
-  std::cout << "nCr = ";
+
+  std::cout << "nCr = "; //print the value
+
   std::cout << nCr(n,r);
+
   std::cout << std::endl;
+  
   return 0;
+}
+
+long Factorial(int no) // calculation form function
+{
+  long factorial=1 ;
+
+  for(int i=1 ; i<=no ; i++)
+  {
+    factorial = factorial*i ;
+  }
+ 
+ return factorial;
+}
+
+long nCr(int n, int r) // calculation from function
+{
+  long nCr=0;
+
+  nCr = Factorial(n)/(Factorial(r)*Factorial(n-r));
+
+  return nCr;
 }
